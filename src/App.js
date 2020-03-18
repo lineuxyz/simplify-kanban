207d19caf833d13839a9763a76f4038d60a137a3
Lineu Pastorelli <lineualberto@gmail.com>
2020-03-18 16:59:29 -0300

    feat: this commit is just to add extra files
    I added some changes inside App.js, and also as I installed some dependencies, I then update
    package.json and also yarn.lock

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { DndProvider } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+
+import GlobalStyle from './styles/global';
+
+import Header from './components/Header';
+import Board from './components/Board';
 
 function App() {
   return (
-    <div className="App">
+    <DndProvider backend={HTML5Backend}>
+      <Header />
+      <Board />
       
-    </div>
+      <GlobalStyle />
+    </DndProvider>
   );
 }
 
